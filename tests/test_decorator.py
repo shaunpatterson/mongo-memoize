ef7c4497ecfe4f10bf582a59fb563506ac167eb5
Ikuya Yamada <ikuya@ikuya.net>
2014-07-15 15:01:00 +0900

    add tests for using capped collection

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -11,11 +11,11 @@ from nose.tools import *
 
 call_count = defaultdict(int)
 db_name = uuid.uuid1().hex
+db_conn = pymongo.Connection()
 
 
 def tearDown():
-    conn = pymongo.Connection()
-    conn.drop_database(db_name)
+    db_conn.drop_database(db_name)
 
 
 @memoize(db_name=db_name)
@@ -24,8 +24,27 @@ def memoized_func():
     return True
 
 
+@memoize(db_name=db_name, collection_name='capped_col', capped=True,
+         capped_size=1000, capped_max=10)
+def memoized_func_capped():
+    call_count['memoized_func_capped'] += 1
+    return True
+
+
 def test_memoize():
     ok_(memoized_func())  # should be called
     time.sleep(0.1)
     ok_(memoized_func())
     eq_(1, call_count['memoized_func'])
+
+
+def test_memoize_capped():
+    ok_(memoized_func_capped())  # should be called
+    time.sleep(0.1)
+    ok_(memoized_func_capped())
+    eq_(1, call_count['memoized_func_capped'])
+
+    col_options = db_conn[db_name]['capped_col'].options()
+    eq_(True, col_options['capped'])
+    eq_(1000.0, col_options['size'])
+    eq_(10, col_options['max'])
